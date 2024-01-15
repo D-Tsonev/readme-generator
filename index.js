06710771c120a9709c9269199514eb32b97187e4
Dimi Tsonev <dconev@gmail.com>
2024-01-15 19:55:51 +0000

    markdown completed

--- index.js
+++ index.js
@@ -5,64 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-  [
-
-    {
-      type: "input",
-      name: "title",
-      message: "What's the name of your project?"
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
   },
-
   {
     type: "input",
     name: "description",
-    message: "Describe about your project:"
-},
-    {
-        type: "input",
-        name: "github",
-        message: "What's your Github username?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What's your email?"
-    },
-    
-    
-    {
-        type: "list",
-        name: "license",
-        message: "What license does your project have?",
-        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
-    },
-    {
-        type: "input",
-        name: "dependencies",
-        message: "Any dependencies to install?",
-        default: "npm i"
-    },
-    {
-        type: "input",
-        name: "test",
-        message: "What command should be run to run tests?",
-        default: "npm test"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "What does the usage of this repo?",
-    },
-    {
-        type: "input",
-        name: "contributors",
-        message: "Who are the contributors of this repo?",
-    }
-
+    message: "Provide a description of your project."
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install the project?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use the project?"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache-2.0", "GPL-3.0", "BSD-3-Clause", "None"]
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can others contribute to your project?"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How do you run tests for your project?"
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
 ];
 
-
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
